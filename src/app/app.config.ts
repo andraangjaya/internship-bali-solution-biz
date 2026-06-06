b5faa9a7416ac6992f6a28a4c11078f5a209b13e
andra <andrabaktiangjaya@gmail.com>
2026-06-07 07:08:13 +0800

    Fixed first load issue and changed icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import {provideRouter, withInMemoryScrolling} from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
